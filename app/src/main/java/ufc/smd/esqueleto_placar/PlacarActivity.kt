3911bb2e05e442f90155ec823221dfa5db3eb0b1
HelanoSM <helano.macambira@gmail.com>
2023-11-06 18:39:14 -0300

    botões times

--- app/src/main/java/ufc/smd/esqueleto_placar/PlacarActivity.kt
+++ app/src/main/java/ufc/smd/esqueleto_placar/PlacarActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
-
+import android.widget.Button
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -27,7 +27,9 @@ class PlacarActivity : AppCompatActivity() {
     lateinit var placar:Placar
     lateinit var tvResultadoJogo: TextView
     var game =0
-    var ultimoPlacar: String = "0 X 0"
+    var golsTimeUm = 0
+    var golsTimeDois = 0
+    var ultimoPlacar = "$golsTimeUm X $golsTimeDois"
     val cronometro = Cronometro(45 * 60, 300.0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,15 +38,16 @@ class PlacarActivity : AppCompatActivity() {
         placar= getIntent().getExtras()?.getSerializable("placar") as Placar
         tvResultadoJogo= findViewById(R.id.tvPlacar)
         tvResultadoJogo.text = ultimoPlacar
-        //Mudar o nome da partida
+
+
         val tvNomePartida=findViewById(R.id.tvNomePartida2) as TextView
         tvNomePartida.text=placar.nome_partida
         ultimoJogos()
         val time1 = intent.getStringExtra("time1")
         val time2 = intent.getStringExtra("time2")
 
-        val nomeTimes1 = findViewById<TextView>(R.id.nomestimes1)
-        val nomeTimes2 = findViewById<TextView>(R.id.nomestimes2)
+        val nomeTimes1 = findViewById<Button>(R.id.nomestimes1)
+        val nomeTimes2 = findViewById<Button>(R.id.nomestimes2)
 
         nomeTimes1.text = time1
         nomeTimes2.text = time2
@@ -58,30 +61,45 @@ class PlacarActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
     }
 
-    fun alteraPlacar (v:View){
-        game++
-        if ((game % 2) != 0) {
-            placar.resultado = ""+game+" vs "+ (game-1)
-        }else{
-            placar.resultado = ""+(game-1)+" vs "+ (game-1)
-            vibrar(v)
+        nomeTimes1.setOnClickListener {
+            golsTimeUm++
+            atualizaPlacar()
         }
-        ultimoPlacar = placar.resultado
-        tvResultadoJogo.text=placar.resultado
+
+        nomeTimes2.setOnClickListener {
+            golsTimeDois++
+            atualizaPlacar()
+        }
+
     }
 
+    private fun atualizaPlacar() {
+        val placarFormatado = String.format("%d X %d", golsTimeUm, golsTimeDois)
+        placar.resultado = placarFormatado
+        ultimoPlacar = placarFormatado
+        tvResultadoJogo.text = placar.resultado
+    }
+
+    fun alteraPlacar(v: View) {
+        game++
+        val placarFormatado = String.format("%d X %d", game, game - 1)
+        placar.resultado = placarFormatado
+        ultimoPlacar = placarFormatado
+        tvResultadoJogo.text = ultimoPlacar
+        vibrar(v)
+    }
 
     fun vibrar (v:View){
         val buzzer = this.getSystemService<Vibrator>()
-         val pattern = longArrayOf(0, 200, 100, 300)
-         buzzer?.let {
-             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                 buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
-             } else {
-                 //deprecated in API 26
-                 buzzer.vibrate(pattern, -1)
-             }
-         }
+        val pattern = longArrayOf(0, 200, 100, 300)
+        buzzer?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
+            } else {
+                //deprecated in API 26
+                buzzer.vibrate(pattern, -1)
+            }
+        }
 
     }
 
@@ -119,7 +137,7 @@ class PlacarActivity : AppCompatActivity() {
                     val oos = ObjectInputStream(dis)
                     val placarAntigo = oos.readObject() as Placar
 
-                    tvResultadoJogo.text = placarAntigo.resultado
+                    tvResultadoJogo.text = "0 X 0"
                 }
             }
         }
@@ -130,7 +148,7 @@ class PlacarActivity : AppCompatActivity() {
         val sharedFilename = "PreviousGames"
         val sp:SharedPreferences = getSharedPreferences(sharedFilename,Context.MODE_PRIVATE)
         var matchStr:String=sp.getString("match1","").toString()
-       // Log.v("PDM22", matchStr)
+        // Log.v("PDM22", matchStr)
         if (matchStr.length >=1){
             var dis = ByteArrayInputStream(matchStr.toByteArray(Charsets.ISO_8859_1))
             var oos = ObjectInputStream(dis)
